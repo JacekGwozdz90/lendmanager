7e13d40142b68f81bad0e9d940e4258cd534ac6a
JacekGwozdz90 <jgwozdz1@gmail.com>
2015-05-10 23:28:34 +0200

    Initial

---
+++ lend/src/main/java/lendmanager/Application.java
@@ -0,0 +1,3 @@
+package lendmanager;
+
+public interface Application {}
---
+++ lend/src/main/java/lendmanager/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package lendmanager.config;
+
+import static org.springframework.context.annotation.ComponentScan.Filter;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+
+import lendmanager.Application;
+
+@Configuration
+@ComponentScan(basePackageClasses = Application.class, excludeFilters = @Filter({Controller.class, Configuration.class}))
+class ApplicationConfig {
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer propertyPlaceholderConfigurer = new PropertyPlaceholderConfigurer();
+		propertyPlaceholderConfigurer.setLocation(new ClassPathResource("/persistence.properties"));
+		return propertyPlaceholderConfigurer;
+	}
+	
+}
