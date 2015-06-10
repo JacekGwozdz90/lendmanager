1c864e0d6f9cf98f8c9502974966ef607788911e
zeuko <zuaskrzynka@gmailcom>
2015-06-10 17:40:16 +0200

    fixed item list view, still needs work

--- lend/src/main/java/lendmanager/controllers/ItemListController.java
+++ lend/src/main/java/lendmanager/controllers/ItemListController.java
@@ -1,13 +1,15 @@
 package lendmanager.controllers;
 
+import java.security.Principal;
+
 import lendmanager.account.AccountRepository;
+import lendmanager.items.Item;
 import lendmanager.items.ItemRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 /**
  * Created by Jacek on 2015-05-22.
@@ -24,7 +26,7 @@ public class ItemListController {
 
     @RequestMapping("/")
     public String showItemAddForm(Model model, Principal principal) {
-        model.addAttribute("itemList", itemRepository.findByPersonLastName(principal.getName()));
+        model.addAttribute("itemList", itemRepository.findAll());
         System.out.println("Add  Item list to show");
         return "lendmanager/listItem";
     }
